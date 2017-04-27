b2222d4bf9be53db3d10b464efee273f01363b6b
cg <trustmore@163.com>
2017-04-27 14:24:18 +0800

    [config]

--- config/apiconfig/config-prod.js
+++ config/apiconfig/config-prod.js
@@ -1,4 +1,4 @@
 export default {
-    apiHost: 'http://54.222.144.175:9013',
-    cdnHost: 'http://54.222.144.175:8332'
+    apiHost: 'http://api.eadoenglish.com',
+    cdnHost: 'http://api.eadoenglish.com'
 };
